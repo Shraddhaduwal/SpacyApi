2f400c8bec49111052dff3a742edd826d7db65a6
Shraddhaduwal <shraddha.duwal@gmail.com>
2020-01-02 17:09:01 +0000

    New route /plot/<string:csv_filename> added

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask
+from flask import Flask, send_file
 from flask_restful import Api
-from db import db
 
 from resources.spacy_resources import Data, AllData
-from models.spacy_models import DataModel
+from data_visualization import visualize
 
 # Init app
 app = Flask(__name__)
@@ -16,6 +15,18 @@ api = Api(app)
 @app.before_first_request  # It is required to create a table first because db does not create one for us
 def create_tables():
     db.create_all()
+
+
+@app.route('/plot/<string:csv_filename>')  # For plotting the data
+def plotting(csv_filename):
+    try:
+        bytes_obj = visualize(csv_filename)
+
+        return send_file(bytes_obj,
+                         attachment_filename='plot.png',
+                         mimetype='image/png')
+    except Exception:
+        return {'message': f'{csv_filename} is empty'}
 
 
 api.add_resource(Data, '/data/<string:text_id>')
